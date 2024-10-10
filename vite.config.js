d38751b11b1aab85971b8adf383e0d71ae479d34
Vasyl Hryn <murphy31337@gmail.com>
2024-10-10 19:05:23 +0300

    Centered images of products

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js', 'resources/css/styles.css'], // Добавь свой CSS файл
+            input: ['resources/js/app.js', 'resources/css/app.css'], // Добавь свой CSS файл
             refresh: true,
         }),
     ],
