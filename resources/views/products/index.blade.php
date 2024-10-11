f3f6dc85f6c954da236efedfcf648cf4705cd447
Vasyl Hryn <murphy31337@gmail.com>
2024-10-11 17:49:22 +0300

    Some improvements for cards

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,42 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Product List</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"> <!-- Подключите свой CSS файл -->
 </head>
 <body>
-@extends('layouts.app')
+@include('layouts.app')
 
-@section('content')
-<div class="row">
-
-    @foreach($products as $product)
-    <div class="col-md-4">
-
-        <div class="card mb-4">
-            @if ($product->image)
-    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" style="width: 100px; height: auto;" />
-@endif
-
-            <div class="card-body">
-                <h5 class="card-title">{{ $product->name }}</h5>
-                <p class="card-text">{{ Str::limit($product->description, 100) }}</p>
-                <p class="card-text"><strong>Price: </strong>${{ $product->price }}</p>
-                <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">View Product</a>
-
+<div class="container">
+    <div class="row">
+        @foreach($products as $product)
+            <div class="col-md-4 mb-4">
+                <div class="card single-product"> <!-- Добавили класс single-product -->
+                    @if ($product->image)
+                        <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="card-img-top" />
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $product->name }}</h5>
+                        <p class="card-text">{{ Str::limit($product->description, 100) }}</p>
+                        <p class="product-price"><strong>Price: </strong>${{ $product->price }}</p>
+                        <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">View Product</a>
+                    </div>
+                </div>
             </div>
-
-        </div>
-
+        @endforeach
     </div>
-
-    @endforeach
-
 </div>
-@endsection
+
 </body>
 </html>
-
-
