088aec31b32bf5ab7e0a16cf993932ce393f7213
Vasyl Hryn <murphy31337@gmail.com>
2024-10-23 21:29:43 +0300

    Added a personalized contact page

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,7 +4,10 @@
 <div class="row">
     <div class="col-md-8 offset-md-2">
         <h1>Contacts</h1>
-        <p>If you have any questions, feel free to contact us at info@store.com.</p>
+        <p>If you have any questions, feel free to contact us at <a href="mailto:murphy31337@gmail.com">murphy31337@gmail.com</a>.</p>
+        <h2>Created by Vasyl Hryn</h2>
+        <a href="https://t.me/ajhvxg">Telegram</a>
+        <a href="https://www.linkedin.com/in/vasyl-h-28b01025a/">LinkedIn</a>
     </div>
 </div>
 @endsection
