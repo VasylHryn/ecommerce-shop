fd4753a85c8b5b5d7e45ba789ac6fce456545bdf
Vasyl Hryn <murphy31337@gmail.com>
2024-11-03 22:44:28 +0200

    Added correct display of the product when you click on it

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,27 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Product Details</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 @extends('layouts.app')
 
 @section('content')
-         @if ($product->image)
-    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" style="width: 100px; height: auto;" />
-@endif
-<div class="row">
-    <div class="col-md-6">
-        <img src="{{ $product->image }}" class="img-fluid" alt="{{ $product->name }}">
-    </div>
-    <div class="col-md-6">
-        <h1>{{ $product->name }}</h1>
-        <p>{{ $product->description }}</p>
-        <h3>${{ $product->price }}</h3>
-        <a href="#" class="btn btn-success">Add to Cart</a>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            @if ($product->image)
+                <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="img-fluid rounded" style="max-height: 400px; object-fit: contain;">
+            @endif
+        </div>
+        <div class="col-md-6">
+            <h1>{{ $product->name }}</h1>
+            <p>{{ $product->description }}</p>
+            <h3>${{ number_format($product->price, 2) }}</h3>
+            <form action="{{ route('cart.add') }}" method="POST">
+                @csrf
+                <input type="hidden" name="product_id" value="{{ $product->id }}">
+                <input type="hidden" name="product_name" value="{{ $product->name }}">
+                <input type="hidden" name="product_price" value="{{ $product->price }}">
+                <button type="submit" class="btn btn-primary btn-lg mt-3">Add to Cart</button>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
